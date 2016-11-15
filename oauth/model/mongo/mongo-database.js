d298f96de8e28ee044779d72f05d7dea8116b593
Etienne Brouillard <etienne.brouillard@outlook.com>
2016-11-15 15:39:31 -0500

    test fixing mongodb connection issues

--- oauth/model/mongo/mongo-database.js
+++ oauth/model/mongo/mongo-database.js
@@ -1,15 +1,30 @@
 const mongoose = require("mongoose");
+mongoose.Promise = require('bluebird');
 const errors = require('../../../tools/errors');
 const Logger = require('../../../tools/logger').Logger;
 
 const logger = new Logger().logger;
 const dbURI = process.env.APPSETTING_APP_MONGO_DB_URL || "mongodb://127.0.0.1:27017/efog-oauth2";
 
-// Create the database connection 
-mongoose.connect(dbURI, {
-    "auto_reconnect": true
-});
-mongoose.Promise = require('bluebird');
+const mdbOptions = {
+    "server": {
+        "auto_reconnect": true,
+        "poolSize": 5,
+        "reconnectInterval": 2000,
+        "autoReconnect": true,
+        "reconnectTries": 10
+    }
+};
+
+const connectWithRetry = function () {
+    return mongoose.connect(dbURI, mdbOptions, function (err) {
+        if (err) {
+            console.error('Failed to connect to mongo on startup - retrying in 5 sec', err);
+            setTimeout(connectWithRetry, 5000);
+        }
+    });
+};
+connectWithRetry();
 
 // CONNECTION EVENTS
 // When successfully connected
@@ -21,6 +36,8 @@ mongoose.connection.on("connected", function () {
 // If the connection throws an error
 mongoose.connection.on("error", function (err) {
     Logger.error(`Mongo DB error: ${err.message}`);
+    mongoose.disconnect();
+    connectWithRetry();
 });
 
 // When the connection is disconnected
