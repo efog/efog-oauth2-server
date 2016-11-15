d1b6677a089ef79bc5d0bdd14a3b39e2a67c6c65
Etienne Brouillard <etienne.brouillard@outlook.com>
2016-11-14 19:40:49 -0500

    mongoose auto-reconnect

--- oauth/model/mongo/mongo-database.js
+++ oauth/model/mongo/mongo-database.js
@@ -6,7 +6,9 @@ const logger = new Logger().logger;
 const dbURI = process.env.APPSETTING_APP_MONGO_DB_URL || "mongodb://127.0.0.1:27017/efog-oauth2";
 
 // Create the database connection 
-mongoose.connect(dbURI);
+mongoose.connect(dbURI, {
+    "auto_reconnect": true
+});
 mongoose.Promise = require('bluebird');
 
 // CONNECTION EVENTS
@@ -18,20 +20,12 @@ mongoose.connection.on("connected", function () {
 
 // If the connection throws an error
 mongoose.connection.on("error", function (err) {
-    Logger.error(err.message);
-    setTimeout(() => { 
-        mongoose.disconnect();
-        mongoose.connect(dbURI);
-    }, 1000);
+    Logger.error(`Mongo DB error: ${err.message}`);
 });
 
 // When the connection is disconnected
 mongoose.connection.on("disconnected", function () {
     Logger.error("Mongoose default connection disconnected");
-    setTimeout(() => {
-        mongoose.disconnect();
-        mongoose.connect(dbURI);
-    }, 1000);
 });
 
 // If the Node process ends, close the Mongoose connection 
